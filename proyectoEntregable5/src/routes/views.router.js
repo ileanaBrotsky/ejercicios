9979142b3fcd6d45ee70b682dff16f808a8d959d
ileana Brotsky <ileana.brotsky-ls@alitaware.com>
2023-07-30 00:31:56 -0300

    update product crud in progress

--- proyectoEntregable5/src/routes/views.router.js
+++ proyectoEntregable5/src/routes/views.router.js
@@ -18,51 +18,65 @@ router.get("/", async (req, res) => {
 
 // Vista para agregar nuevo producto
 router.get("/create", async (req, res) => {
-  res.render('create', {})
+  res.render("create", {});
 });
 //Accion de agregar producto nuevo
-router.post('/create', async (req, res) => {
-  const productNew = req.body
-  console.log({ productNew })
-  const productGenerated = new productModel(productNew)
-  await productGenerated.save()
-  console.log({ productGenerated });
-  res.redirect('/')
-})
+router.post("/create", async (req, res) => {
+  const productNew = req.body;
+  const productGenerated = new productModel(productNew);
+  try {
+    await productGenerated.save();
+    console.log({ productGenerated });
+    res.redirect("/");
+  } catch (error) {
+    console.log("cannot create products", error);
+  }
+});
 // Vista para modificar producto existente
 router.get("/update/:code", async (req, res) => {
   console.log("el req es", req.params);
-  const code = req.params.code
+  const code = req.params.code;
   try {
-    const productSelected = await productModel
-      .findOne({ code: code })
-      .lean().exec();
+    const productSelected = await productModel.findOne({ code: code });
     console.log("obtenido con exito", productSelected);
     res.render("update", productSelected);
   } catch (error) {
-    console.log("cannot get products with mongoose", error);
+    console.log("cannot update products", error);
   }
 });
 
 //Modificar un producto existente
 router.put("/update/:code", async (req, res) => {
-  console.log("el body que llega es", req.body);
-  //ME FALTA TERMINAR
+  let code = req.body.code;
+  let productToUpdate = req.body;
+  // console.log("el code es", code)
+  // console.log("el update es",productToUpdate)
+  if (
+    !productToUpdate.code ||
+    !productToUpdate.description ||
+    !productToUpdate.price ||
+    !productToUpdate.category ||
+    !productToUpdate.stock
+  ) {
+    return res.send({ status: "error", error: "Valores incompletos" });
+  }
+  let result = await productModel.updateOne({ code: code }, productToUpdate);
+  res.send({ status: "sucess", payload: result }).redirect("/");
 });
-// Eliminar un producto existente
-router.get('/delete/:code', async (req, res) => {
-  const code = req.params.code
 
-  await productModel.deleteOne({ code: code })
-  res.redirect('/')
-})
+// Eliminar un producto existente
+router.get("/delete/:code", async (req, res) => {
+  const code = req.params.code;
+
+  await productModel.deleteOne({ code: code });
+  res.redirect("/");
+});
 
 //=============================================================//
 //chat con websockets
 router.get("/chat", (req, res) => {
   res.render("chat", { style: "index.css" });
 });
-
 
 // router.get('/realtimeproducts',async (req, res)=>{
 //     try {
